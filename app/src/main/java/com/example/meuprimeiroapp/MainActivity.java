f2524378b964a3eb6776c66b3fc5875f46f8ad48
Deyvison Cruz <deyvisoncruzoficial@gmail.com>
2023-05-16 09:46:22 -0300

    Tipos de escala
    Explicação sobre os diversos tipos de valores que podemos definir para o atributo scaleType do elemento ImageView.

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.java
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.meuprimeiroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Matrix;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_matrix;
+    private Matrix matrix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_matrix = findViewById(R.id.iv_matrix);
+        matrix = new Matrix();
+        matrix.setSkew(2,2);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            iv_matrix.setAnimationMatrix(matrix);
+        }
     }
 }
